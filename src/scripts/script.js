ca4826f4b23f6c1ab9904e1c82203b96ed6b8abf
Дмитрий Жур <tehzienergy@gmail.com>
2016-09-17 00:22:10 +0300

    release 1.0

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,11 +8,24 @@ $(document).ready(function() {
     arrows: false,
     slidesToShow: 1,
     autoplay: true,
+    infinite: true
   });
   
   $('.locomotives__list').slick({
     slidesToShow: 3,
     autoplay: false,
     infinite: false,
+  });
+  
+  $('.modal').perfectScrollbar();
+  
+  $('.story__btn').click(function(e) {
+    e.preventDefault();
+    $.fancybox.close();
+  });
+  
+  $('.tibet__btn').click(function(e) {
+    e.preventDefault();
+    $.fancybox.close();
   });
 });
