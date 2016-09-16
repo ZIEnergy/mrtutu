a2719924b33cc7379e53c6e6da2930a169f35dc8
Дмитрий Жур <tehzienergy@gmail.com>
2016-09-16 19:58:12 +0300

    full page not refactored

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+  
   $('.fancybox').fancybox({
     padding: 0
+  });
+  
+  $('.banner').slick({
+    arrows: false,
+    slidesToShow: 1,
+    autoplay: false,
   });
 });
